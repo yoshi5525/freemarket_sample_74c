391e71271b82232886eeed0474604511971ba0c4
yoshi5525 <mreage29@gmail.com>
2020-05-11 10:51:45 +0900

    s3画像投稿調整2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,6 +15,6 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1'
     }
     config.fog_directory  = 'chat-space55555'
-    config.asset_host = 'https://chat-space55555.s3-ap-northeast-1.amazonaws.com'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space55555'
   end
 end
