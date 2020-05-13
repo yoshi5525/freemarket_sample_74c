71c1bf5b51c22e6e0ae6cc71d33300a9e5a9ab5f
yoshi5525 <mreage29@gmail.com>
2020-05-13 20:15:31 +0900

    画像投稿機能の実装（コメントアウトの削除）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,14 +6,11 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     @item.images.new
-    # @item = Item.includes(:images).order('created_at DESC')
   end
 
   def create
     @item = Item.new(item_params)
-    # if @item.images.length == 1..10 && @item.save
-    if  @item.valid? && @item.images.length == 1..10
-      @item.save
+    if @item.save
       redirect_to @item
     else
       render :new
@@ -38,6 +35,5 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:name, :introduction, :condition, :area_id, :size, :price, :preparation_day, :postage, images_attributes: [:image])
-    # (:category_id, :brand_id).merge(seller_id: current_user.id)
   end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,5 @@
 class Image < ApplicationRecord
   belongs_to :item
 
-  validates :image, presence: true
-
   mount_uploader :image, ImageUploader
 end
