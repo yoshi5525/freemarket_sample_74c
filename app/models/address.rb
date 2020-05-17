c88ce62efb2724bf670b255285cc3ee9f5d07e15
kazu-daimar <daima.exe@gmail.com>
2020-05-17 20:01:30 +0900

    バリデーション追加
    userモデルとaddressモデルにバリデーションを追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,9 @@
 class Address < ApplicationRecord
-  validates :post_code, :prefecture, :city, :address, :apartment, presence: true
+  validates :first_name, :family_name, :first_name_kana, :family_name_kana, :post_code, :prefecture, :city, :address, presence: true
+  validates :first_name, :family_name,           format: {with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name_kana, :family_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :tel_number, uniqueness: true,       format: {with: /\A\d{10,11}\z/, allow_blank: true}
+  validates :post_code,                          format: {with: /\A\d{3}[-]\d{4}\z/}
+
   belongs_to :user, optional: true
 end
