1f1c832301f48045c7acb5661d7b9f70ba0832ce
kazu-daimar <daima.exe@gmail.com>
2020-05-18 09:38:52 +0900

    バリデーション追加
    addressモデルとuserモデルにバリデーション追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,7 +2,7 @@ class Address < ApplicationRecord
   validates :first_name, :family_name, :first_name_kana, :family_name_kana, :post_code, :prefecture, :city, :address, presence: true
   validates :first_name, :family_name,           format: {with: /\A[ぁ-んァ-ン一-龥]/}
   validates :first_name_kana, :family_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
-  validates :tel_number, uniqueness: true,       format: {with: /\A\d{10,11}\z/, allow_blank: true}
+  validates :tel_number,                         format: {with: /\A\d{10,11}\z/}, uniqueness: true, allow_blank: true
   validates :post_code,                          format: {with: /\A\d{3}[-]\d{4}\z/}
 
   belongs_to :user, optional: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   validates :first_name, :family_name,             format: {with: /\A[ぁ-んァ-ン一-龥]/}
   validates :first_name_kana, :family_name_kana,   format: {with: /\A[ァ-ヶー－]+\z/}
   validates :birth_year, :birth_month, :birth_day, format: {with: /\A[0-9]+\z/}, numericality: { only_integer: true , greater_than: 0}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
   has_many :addresses, dependent: :destroy
   has_many :items,     dependent: :destroy
