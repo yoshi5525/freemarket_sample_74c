81bedb070a8d9547b009c5d3a2d40f335ab095d9
kazu-daimar <daima.exe@gmail.com>
2020-05-18 09:40:28 +0900

    住所情報に氏名が必要だったため追加

--- db/migrate/20200512070558_create_addresses.rb
+++ db/migrate/20200512070558_create_addresses.rb
@@ -1,13 +1,17 @@
 class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
-      t.string     :post_code,  null: false
-      t.string     :prefecture, null: false
-      t.string     :city,       null: false
-      t.string     :address,    null: false
-      t.string     :apartment,  null: false
-      t.string     :tel_number, null: false
-      t.references :user,       null: false, foreign_key: true
+      t.string     :first_name,       null: false
+      t.string     :family_name,      null: false
+      t.string     :first_name_kana,  null: false
+      t.string     :family_name_kana, null: false
+      t.string     :post_code,        null: false
+      t.string     :prefecture,       null: false
+      t.string     :city,             null: false
+      t.string     :address,          null: false
+      t.string     :apartment,        null: false
+      t.string     :tel_number,       null: false
+      t.references :user,             null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_070558) do
+ActiveRecord::Schema.define(version: 2020_05_12_100424) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "family_name", null: false
+    t.string "first_name_kana", null: false
+    t.string "family_name_kana", null: false
     t.string "post_code", null: false
     t.string "prefecture", null: false
     t.string "city", null: false
