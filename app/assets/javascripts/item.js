1503ed4dee990169ec8ffa696319add8b37001af
yoshi5525 <63286009+yoshi5525@users.noreply.github.com>
2020-05-19 14:54:06 +0900

    Update item.js

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -34,6 +34,11 @@ $(document).on('turbolinks:load', ()=> {
 
   let fileIndex = [0,1,2,3,4,5,6,7,8,9];
 
+  if ($('.js-file').length == 0) {
+    $("#image-label").attr("for", `item_images_attributes_${fileIndex[0]}_image`);
+    $('.new-wrapper__main__image-box').append(buildFileField(fileIndex[0]));
+  };
+  
   $('.new-wrapper__main__image-box').on('change', '.js-file', function(e) {
     const targetIndex = $(this).parent().data('index');
     const file = e.target.files[0];
