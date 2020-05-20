cf06172cc94c22fa99759d4e80aca98bf01c7545
yoshi5525 <63286009+yoshi5525@users.noreply.github.com>
2020-05-20 12:14:11 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,17 +4,15 @@ class Item < ApplicationRecord
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 
-  with_options presence: true do
-    validates :name, length: { minimum: 1, maximum: 40 }
-    validates :introduction, length: { minimum: 1, maximum: 1000 }
-    validates :condition
-    validates :area
-    validates :price, numericality: { only_integer: true , greater_than: 299, less_than: 10000000 }
-    validates :preparation_day
-    validates :postage
-    validates :status
-    validates :images
-  end
+  validates :name, length: { minimum: 1, maximum: 40 }, presence: true
+  validates :introduction, length: { minimum: 1, maximum: 1000 }, presence: true
+  validates :condition, presence: true
+  validates :area, presence: true
+  validates :price, numericality: { only_integer: true , greater_than: 299, less_than: 10000000 }, presence: true
+  validates :preparation_day, presence: true
+  validates :postage, presence: true
+  validates :status, presence: true
+  
   validates_associated :images
   enum preparation_day: [:short, :middle, :long]
   enum postage: [:including, :noincluding]
