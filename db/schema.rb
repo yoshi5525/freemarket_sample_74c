223f2d95af822c945389b5e75be2f15f7457e802
nana0822 <63431900+nana0822@users.noreply.github.com>
2020-05-23 18:07:47 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_17_053403) do
+ActiveRecord::Schema.define(version: 2020_05_17_140021) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_05_17_053403) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
+  end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
