5e59617e856e420e5d13486a871193da3a01a4eb
makoto323 <63290395+makoto323@users.noreply.github.com>
2020-05-27 20:58:55 +0900

    不要な記述を削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,13 +31,8 @@ class ItemsController < ApplicationController
     grandchild_category = @item.category
     child_category = grandchild_category.parent
 
-    @category_parent_array = []
     @category_parent_array = Category.where(ancestry: nil)
-
-    @category_children_array = []
     @category_children_array = Category.where(ancestry: child_category.ancestry)
-
-    @category_grandchildren_array = []
     @category_grandchildren_array = Category.where(ancestry: grandchild_category.ancestry)
   end
 
@@ -48,13 +43,8 @@ class ItemsController < ApplicationController
       grandchild_category = @item.category
       child_category = grandchild_category.parent
 
-      @category_parent_array = []
       @category_parent_array = Category.where(ancestry: nil)
-
-      @category_children_array = []
       @category_children_array = Category.where(ancestry: child_category.ancestry)
-
-      @category_grandchildren_array = []
       @category_grandchildren_array = Category.where(ancestry: grandchild_category.ancestry)
       render :edit
     end
